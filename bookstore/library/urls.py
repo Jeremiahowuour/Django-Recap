22519deff104ef25db09695d31b195f5b4372ad2
sakamw <shanmwangi2020@gmail.com>
2025-11-28 22:51:45 +0300

    CRUD - Read books, Read one book and update a book

--- bookstore/library/urls.py
+++ bookstore/library/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create-book', views.createBook, name='createBook'),
-    path('read-books', views.readBooks, name='readBooks')
+    path('read-books', views.readBooks, name='readBooks'),
+    path('read-one-book/<str:pk>', views.readOneBook, name='readOneBook'),
+    path('update-book/<str:pk>', views.updateBook, name='updateBook'),
+    
 ]
--- bookstore/library/views.py
+++ bookstore/library/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .forms import BookForm
 from .models import Book
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'library/home.html')
+	return render(request, "library/home.html")
 
 def createBook(request):
     form = BookForm()
@@ -21,6 +21,23 @@ def createBook(request):
     return render(request, "library/form.html", context)
 
 def readBooks (request):
-     books = Book.objects.all()
-     context ={'books':books}
-     return render(request, "library/books.html", context)
+    books = Book.objects.all()
+    context ={'books':books}
+    return render(request, "library/books.html", context)
+
+def readOneBook(request, pk):
+    book = Book.objects.get(id=pk)
+    context = {"book":book}
+    return render (request, "library/book.html", context)
+
+def updateBook(request, pk):
+    book = Book.objects.get(id=pk)
+    form = BookForm (instance= book)
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance = book)
+        if form.is_valid():
+            form.save()
+            return redirect("readBooks")
+    context = {"form": form}
+    return render(request, "library/form.html", context)
